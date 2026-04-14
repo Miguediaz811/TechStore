bbab6cebb353dedb19b9fa8c346e08a63686a20b
Kevin03 <santimosquerav@gmail.com>
2026-04-14 12:50:39 -0500

    feat: modified controllers and JwtService

--- src/main/java/com/proyecto/TechStore/controllers/PurchaseController.java
+++ src/main/java/com/proyecto/TechStore/controllers/PurchaseController.java
@@ -4,7 +4,6 @@ import com.proyecto.TechStore.dto.MessageResponseDTO;
 import com.proyecto.TechStore.dto.PurchaseRequestDTO;
 import com.proyecto.TechStore.entity.Purchase;
 import com.proyecto.TechStore.service.PurchaseService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,15 +19,13 @@ public class PurchaseController {
     private final PurchaseService purchaseService;
 
     @PostMapping
-    public ResponseEntity<MessageResponseDTO> createPurchase(@Valid @RequestBody PurchaseRequestDTO request, HttpServletRequest httpRequest) {
+    public ResponseEntity<MessageResponseDTO> createPurchase(@Valid @RequestBody PurchaseRequestDTO request, @RequestAttribute("role") String role, @RequestAttribute("id") Long userId) {
         try {
-            String role = (String) httpRequest.getAttribute("role");
             if (!"client".equals(role)) {
                 MessageResponseDTO error = new MessageResponseDTO();
                 error.setMessage("Solo los clientes pueden realizar compras");
                 return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
             }
-            Long userId = (Long) httpRequest.getAttribute("id");
             return ResponseEntity.status(HttpStatus.CREATED).body(purchaseService.createPurchase(request, userId));
         } catch (Exception e) {
             e.printStackTrace();
@@ -39,9 +36,8 @@ public class PurchaseController {
     }
 
     @GetMapping("/my-history")
-    public ResponseEntity<List<Purchase>> getMyPurchases(HttpServletRequest httpRequest) {
+    public ResponseEntity<List<Purchase>> getMyPurchases(@RequestAttribute("id") Long userId) {
         try {
-            Long userId = (Long) httpRequest.getAttribute("id");
             return ResponseEntity.ok(purchaseService.getMyPurchases(userId));
         } catch (Exception e) {
             e.printStackTrace();
@@ -50,9 +46,8 @@ public class PurchaseController {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Purchase>> getAllPurchases(HttpServletRequest httpRequest) {
+    public ResponseEntity<List<Purchase>> getAllPurchases(@RequestAttribute("role") String role) {
         try {
-            String role = (String) httpRequest.getAttribute("role");
             if (!"admin".equals(role)) {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
             }
